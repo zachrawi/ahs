956ded6ec34dec9ba2af00788f1b762086407d6b
Zachrawi Al Farizi <zachrawi@ngoprekweb.com>
2020-04-30 22:27:00 +0700

    Add merchant_id

---
+++ database/migrations/20200430151557-add-merchant-id-to-users-table.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Users', 'merchant_id', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Merchants',
+        key: 'id',
+        as: 'merchant_id'
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Users', 'merchant_id');
+  }
+};
